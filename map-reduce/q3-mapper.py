0b4eed97762c6c05adaacc6c6d0bb64606f3ee78
Yifeng Huang <ivanhyf@gmail.com>
2014-03-28 12:12:27 -0400

    update q3 mapper

--- map-reduce/q3-mapper.py
+++ map-reduce/q3-mapper.py
@@ -8,7 +8,7 @@ def main(argv):
 	for line in sys.stdin:
 		#data_string = json.dumps(line)
 		decoded = json.loads(line)
-		if not 'retweeted_status' in decoded or not user in decoded['retweeted_status']:
+		if not 'retweeted_status' in decoded or not 'user' in decoded['retweeted_status']:
 			continue
 		orig_user = decoded['retweeted_status']['user']['id_str']
 		current_user = decoded['user']['id_str']
